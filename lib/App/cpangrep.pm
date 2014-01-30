897ed25134237a28c12ee58385a61110c2dcb157
Thomas Sibley <tsibley@cpan.org>
2014-01-30 05:20:06 +0000

    Import of TSIBLEY/App-cpangrep-0.06 from CPAN.
    gitpan-cpan-distribution: App-cpangrep
    gitpan-cpan-version:      0.06
    gitpan-cpan-path:         TSIBLEY/App-cpangrep-0.06.tar.gz
    gitpan-cpan-author:       TSIBLEY
    gitpan-cpan-maturity:     released

--- lib/App/cpangrep.pm
+++ lib/App/cpangrep.pm
@@ -6,7 +6,7 @@ use 5.008_005;
 use utf8;
 use open OUT => qw< :encoding(UTF-8) :std >;
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 use Config;
 use URI::Escape qw(uri_escape);
@@ -154,6 +154,24 @@ sub display {
 
                 my ($start, $len) = @{$match->{match}};
                 $len -= $start;
+
+                # XXX TODO: Track down the grep.cpan.me api bug that causes
+                # this.  An example that fails for me today:
+                #
+                #   cpangrep dist:App-Prefix file:Changes '^\s*\[.+?\]\s*$'
+                #
+                # -trs, 29 Jan 2014
+                if (length $snippet < $start + $len) {
+                    warn colored("API returned an out of bounds match; skipping! (use --debug to see details)", "RED"),
+                         color("reset"), "\n";
+                    if ($DEBUG) {
+                        require Data::Dumper;
+                        debug("snippet: «$snippet» (length ", length $snippet, ")");
+                        debug("reported match starts at «$start», length «$len» (ends at «@{[$start+$len]}»)");
+                        debug("raw match response: ", Data::Dumper::Dumper($match));
+                    }
+                    next;
+                }
 
                 substr($snippet, $start, $len) = colored(substr($snippet, $start, $len), "BOLD RED");
 
